f7027a76fe431307e55deb0f0d0b04a676cd0663
Matteo Arnese <68701124+xMattMaster@users.noreply.github.com>
2022-02-25 13:09:48 +0100

    Typo correction

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # giardinomiranda
-Il giardino biodiverso del Liceo Miranda è un progetto creato da Matteo Arnese come elaborato PCTO per l'Esame di Stato A.S 2020/2021, è oper source ed è stato creato con ReactJS e npm.
+Il giardino biodiverso del Liceo Miranda è un progetto creato da Matteo Arnese come elaborato PCTO per l'Esame di Stato A.S 2020/2021, è open source ed è stato creato con ReactJS e npm.
--- src/footer.js
+++ src/footer.js
@@ -7,7 +7,7 @@ export default function Footer(props) {
     <Container class="footer">
         <Grid container spacing={3} justify="space-around">
             <Grid item xs={12} md={6}>
-              <p onClick={()=>{window.open('https://github.com/xMattMaster/giardinomiranda')}}>Il giardino biodiverso del Liceo Miranda è un progetto creato da Matteo Arnese come elaborato PCTO per l'Esame di Stato A.S 2020/2021, è oper source ed è stato creato con ReactJS e npm. La repo è disponibile al seguente link: https://github.com/xMattMaster/giardinomiranda</p>
+              <p onClick={()=>{window.open('https://github.com/xMattMaster/giardinomiranda')}}>Il giardino biodiverso del Liceo Miranda è un progetto creato da Matteo Arnese come elaborato PCTO per l'Esame di Stato A.S 2020/2021, è open source ed è stato creato con ReactJS e npm. La repo è disponibile al seguente link: https://github.com/xMattMaster/giardinomiranda</p>
               <p>È stato indispensabile il supporto della professoressa Mariagrazia Del Prete, che da decenni si impegna a portare avanti l'iniziativa del giardino biodiverso del Miranda con passione e costanza, facendo appassionare alla sua causa centinaia di studenti ogni anno, tra cui me. Il suo aiuto e la sua collaborazione sono stati vitali per la realizzazione di questo progetto. I suoi costanti sforzi, in ambito didattico e non, durante i miei cinque anni trascorsi con lei hanno contribuito alla realizzazione di tutto ciò. Con questo progetto spero di ringraziarla di tutto.</p>
             </Grid>
             <Grid item xs={12} md={6}>
